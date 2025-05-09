7db634dba43981ad234417106965a21b750db60c
DonaldXoftDev <chibuezeokon1007@mail.com>
2025-05-08 19:21:17 -0700

    upload my project files

---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+import arts
+
+options = {
+    'rock': arts.rock,
+    'paper' : arts.paper,
+    'scissors': arts.scissors,
+}
+
+winning_options = {
+    'rock': arts.scissors,
+    'paper': arts.rock,
+    'scissors':  arts.paper,
+}
+
+
+def game_logic():
+    player_choice = input('enter a choice from either rock, paper, scissors: ').lower()
+    if player_choice not in options:
+        print('invalid choice chosen!')
+        return game_logic()
+
+    player_image = options[player_choice]
+    computer_choice = random.choice(list(options.values()))
+
+    print(f'player chose: \n {player_image}')
+    print(f'Computer chose:\n {computer_choice}')
+
+    for key, value in winning_options.items():
+        if player_image ==  computer_choice:
+            print("It's a tie!")
+            break
+        if player_choice == key and computer_choice == value:
+            print('You win')
+        elif player_choice == key and computer_choice != value:
+            print('You lose')
+
+    play_again = input('Do you want to play again'
+                       '? ').lower()
+    if play_again == 'yes':
+        game_logic()
+    elif play_again == 'no':
+        print(print('Goodbye!'))
+
+
+game_logic()
+
